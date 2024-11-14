e13bded4f7f8ef3b8965979486c50de41dbf5da3
Azerty <zinkacartinka765@gmail.com>
2024-11-15 01:16:36 +0300

    add javadoc

--- src/javadoc/package-search-index.js
+++ src/javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.study.BlogPlatform.configuration"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.study.BlogPlatform.controllers"}];updateSearchResults();
--- src/javadoc/type-search-index.js
+++ src/javadoc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.study.BlogPlatform.configuration","l":"WebConfig"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.study.BlogPlatform.controllers","l":"BlogController"}];updateSearchResults();
